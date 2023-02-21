28504f3e7b1097c07161dec05da11e5e46a3dd09
YashwantNG <gawandeyashwant7@gmail.com>
2023-02-21 22:17:49 +0530

    update

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import './GigCard.scss'
-const GigCard = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+import "./GigCard.scss";
+const GigCard = ({ card }) => {
   return (
-    <div>GigCard</div>
-  )
-}
+    <Link to="/gig/123">
+      <div className="gigCard">
+        <img src={card.img} alt="" />
+        <div className="info">
+          <div className="user">
+            <img src={card.pp} alt="" />
+            <span>{card.username}</span>
+          </div>
+          <p>{card.desc}</p>
+          <div className="star">
+            <img src="./img/star.png" alt="" />
+            <span>{card.star}</span>
+          </div>
+        </div>
+        <hr />
+        <div className="details">
+          <img src="./img/heart.png" alt="" />
+          <div className="price">
+            <span>STARTING AT</span>
+            <h2>${card.price}</h2>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-export default GigCard
+export default GigCard;
--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './Gigs.scss'
+import React from "react";
+import "./Gigs.scss";
 
 const Gigs = () => {
   return (
-    <div>Gigs</div>
-  )
-}
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">Liverr > Graphics & Design ></span>
+        <h1>AI ARTIST</h1>
+        <p>
+          Explore the boundaries of art and technology with Liverr's AI artists
+        </p>
+      </div>
+      <div className="menu">
+        <div className="left">
+          <span>Budget</span>
+          <input type="text" placeholder="min" />
+          <input type="text" placeholder="max" />
+          <button>Apply</button>
+        </div>
+        <div className="right">
+          <span className="sortBy">Sort By</span>
+          <span className="sortType">Bestselling</span>
+          <img src="./img.down.png" alt="" />
+          <div className="rightMenu">
+            <span>Newest</span>
+            <span>Best Selling</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Gigs
+export default Gigs;
