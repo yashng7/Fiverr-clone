e90a7fa87699e2d73e130f4729c38a5a7297830f
YashwantNG <gawandeyashwant7@gmail.com>
2023-03-11 16:09:29 +0530

    Change Directory to client

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import Featured from "../../components/featured/Featured";
-import Slide from "../../components/slide/Slide";
-import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
-import { cards, projects } from "../../data";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
 import CatCard from "../../components/catCard/CatCard";
 import ProjectCard from "../../components/projectCard/ProjectCard";
-
+import { cards, projects } from "../../data";
 const Home = () => {
   return (
     <div className="Home">
       <Featured />
       <TrustedBy />
-      <Slide>
+      <Slide slidesToShow={5} arrowsScroll={5}>
         {cards.map((card) => (
           <CatCard key={card.id} card={card} />
         ))}
