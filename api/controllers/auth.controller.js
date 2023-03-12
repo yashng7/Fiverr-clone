1cc722cc1208a61aa34f2b365ab4cddfcd53c7d9
Your Name <your_email@example.com>
2023-03-12 23:22:25 +0530

    update authentication controller to handle token-based authentication.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -21,11 +22,24 @@ export const login = async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
     if (!user) return res.status(404).send("User not Found");
 
+    const token = Jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
+
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect) return res.status(400).send("Wrong password or username!");
 
     const { password, ...info } = user._doc;
-    res.status(200).send(info);
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Something went wrong");
