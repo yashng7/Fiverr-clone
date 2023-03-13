64f7d1b912f0ebd9e20ac32787d92e537a0bafd2
Your Name <your_email@example.com>
2023-03-13 21:40:04 +0530

    Add login functionality for user authentication with styles

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import './Login.scss'
+import React, { useState } from "react";
+import "./Login.scss";
+// import newRequest from "../../utils/newRequest";
+// import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const Login = () => {
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/login", {
+        username,
+        password,
+      });
+      console.log(res.data);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && error}
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
