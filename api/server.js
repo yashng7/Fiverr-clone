f529969b5a5b090f190ef440e03d3e607fd4e854
Your Name <your_email@example.com>
2023-03-14 20:41:51 +0530

    Fix issue with credentials

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ const connect = async () => {
     console.log(error);
   }
 };
-app.use(cors({ origin: "http://localhost:5173", credential: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true,  }));
 app.use(express.json());
 app.use(cookieParser());
 
