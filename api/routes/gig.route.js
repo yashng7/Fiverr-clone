335a8ead31f91546ea1cc4b3bffd6ed919bc0d40
Your Name <your_email@example.com>
2023-03-15 23:26:26 +0530

    Update routes

--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,3 +1,15 @@
 import express from "express";
+import {
+  createGig,
+  deleteGig,
+  getGig,
+  getGigs,
+} from "../controllers/gig.controller.js";
+import { verifyToken } from "../midelware/jwt.js";
+
 const router = express.Router();
+router.post("/", verifyToken, createGig);
+router.delete("/:id", verifyToken, deleteGig);
+router.get("/single/:id", getGig);
+router.get("/", getGigs);
 export default router;
