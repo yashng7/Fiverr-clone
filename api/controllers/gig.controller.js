137b6bd9b5976ca268d8e93082d816aa1f428302
Yashwant Gawande <gawandeyashwant7@gmail.com>
2023-03-16 23:03:52 +0530

    Update gig controller to create delete get gig /gigs

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -17,44 +17,45 @@ export const createGig = async (req, res, next) => {
     next(err);
   }
 };
+
 export const deleteGig = async (req, res, next) => {
   try {
     const gig = await Gig.findById(req.params.id);
+
     if (gig.userId !== req.userId)
       return next(createError(403, "You can delete only your gig!"));
 
     await Gig.findByIdAndDelete(req.params.id);
-    res.status(200).send("Gig has been deleted!");
+    res.status(200).send("Gig has been deleted");
   } catch (err) {
     next(err);
   }
 };
+
 export const getGig = async (req, res, next) => {
   try {
     const gig = await Gig.findById(req.params.id);
-    if (!gig) next(createError(404, "Gig not found!"));
+    if (!gig) next(createError(404, "Gig not found"));
     res.status(200).send(gig);
   } catch (err) {
     next(err);
   }
 };
+
 export const getGigs = async (req, res, next) => {
   const q = req.query;
   const filters = {
     ...(q.userId && { userId: q.userId }),
     ...(q.cat && { cat: q.cat }),
     ...((q.min || q.max) && {
-      price: {
-        ...(q.min && { $gt: q.min }),
-        ...(q.max && { $lt: q.max }),
-      },
+      price: { ...(q.min && { $gt: q.min }), ...(q.max && { $lt: q.max }) },
     }),
     ...(q.search && { title: { $regex: q.search, $options: "i" } }),
   };
   try {
-    const gigs = await Gig.find(filters).sort({ [q.sort]: -1 });
+    const gigs = await Gig.find(filters);
     res.status(200).send(gigs);
   } catch (err) {
-    next(err);
+    next;
   }
 };
