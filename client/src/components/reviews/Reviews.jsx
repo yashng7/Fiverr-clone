303349596a88ef6957c8e4904002fe710e45a1e9
Yashwant Gawande <gawandeyashwant7@gmail.com>
2023-03-18 22:38:52 +0530

    Add reviews section for reviews

---
+++ client/src/components/reviews/Reviews.jsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import newRequest from "../../../utils/newRequest";
+import Review from "../review/Review";
+import "./Reviews.scss";
+
+const Reviews = ({ gigId }) => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["reviews"],
+    queryFn: () =>
+      newRequest.get(`/reviews/${gigId}`).then((res) => {
+        return res.data;
+      }),
+  });
+  return (
+    <div className="reviews">
+      <h2>Reviews</h2>
+      {isLoading
+        ? "loading"
+        : error
+        ? "Something went wrong!"
+        : data.map((review) => <Review key={review._id} review={review} />)}
+    </div>
+  );
+};
+
+export default Reviews;
