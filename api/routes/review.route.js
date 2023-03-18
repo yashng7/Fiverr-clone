3d0db1d65f5d5e1745b807898b832a48c5bb3c0a
Your Name <your_email@example.com>
2023-03-18 22:33:14 +0530

    Add router for review

--- api/routes/review.route.js
+++ api/routes/review.route.js
@@ -1,3 +1,14 @@
 import express from "express";
+import {
+  createReview,
+  getReview,
+  deleteReview,
+} from "../controllers/review.controller.js";
+
 const router = express.Router();
+
+router.post("/", verifyToken, createReview);
+router.get("/:id", getReview);
+router.post("/:id", deleteReview);
+
 export default router;
