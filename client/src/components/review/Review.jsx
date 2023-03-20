36f53edbf12067237f789a3006cfd66f10f60819
Your Name <your_email@example.com>
2023-03-20 23:59:12 +0530

    Update review

--- client/src/components/review/Review.jsx
+++ client/src/components/review/Review.jsx
@@ -1,51 +1,52 @@
-import React from 'react'
-import "./Review.scss"
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import newRequest from "../../../utils/newRequest";
+import "./Review.scss";
 
-const Review = () => {
+const Review = ({ review }) => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: [review.userId],
+    queryFn: () =>
+      newRequest.get(`/users/${review.userId}`).then((res) => {
+        return res.data;
+      }),
+  });
+
   return (
     <div className="review">
+      {isLoading ? (
+        "loading"
+      ) : error ? (
+        "Something went wrong"
+      ) : (
         <div className="user">
-          <img
-            className="pp"
-            src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-          />
+          <img className="pp" src={data.img || "/img/noavatar.jpg"} alt="" />
           <div className="info">
-            <span>Garner David</span>
+            <span>{data.username}</span>
             <div className="country">
-              <img
-                src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                alt=""
-              />
-              <span>United States</span>
+              <span>{data.country}</span>
             </div>
           </div>
         </div>
-        <div className="stars">
-          <img src="/img/star.png" alt="" />
-          <img src="/img/star.png" alt="" />
-          <img src="/img/star.png" alt="" />
-          <img src="/img/star.png" alt="" />
-          <img src="/img/star.png" alt="" />
-          <span>5</span>
-        </div>
-        <p>
-          I just want to say that art_with_ai was the first, and after this, the
-          only artist Ill be using on Fiverr. Communication was amazing, each
-          and every day he sent me images that I was free to request changes to.
-          They listened, understood, and delivered above and beyond my
-          expectations. I absolutely recommend this gig, and know already that
-          Ill be using it again very very soon
-        </p>
-        <div className="helpful">
-          <span>Helpful?</span>
-          <img src="/img/like.png" alt="" />
-          <span>Yes</span>
-          <img src="/img/dislike.png" alt="" />
-          <span>No</span>
-        </div>
+      )}
+      <div className="stars">
+        {Array(review.star)
+          .fill()
+          .map((item, i) => (
+            <img src="/img/star.png" alt="" />
+          ))}
+        <span>{review.star}</span>
       </div>
-  )
-}
+      <p>{review.desc}</p>
+      <div className="helpful">
+        <span>Helpful?</span>
+        <img src="/img/like.png" alt="" />
+        <span>Yes</span>
+        <img src="/img/dislike.png" alt="" />
+        <span>No</span>
+      </div>
+    </div>
+  );
+};
 
-export default Review
+export default Review;
