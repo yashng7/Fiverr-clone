c16b010e2406a03d4404ca3932736f70952b3374
Your Name <your_email@example.com>
2023-03-21 23:48:21 +0530

    Add order route

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,3 +1,10 @@
 import express from "express";
+import { verifyToken } from "../midelware/jwt.js";
+import { getOrder, createOrder } from "../controllers/order.controller.js";
+
 const router = express.Router();
+
+router.post("/:gigId", verifyToken, createOrder);
+router.get("/", verifyToken, getOrder);
+
 export default router;
