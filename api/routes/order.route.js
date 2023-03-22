697bd98dcd8f4b5759116814760e1a2046b91340
Yashwant Gawande <gawandeyashwant7@gmail.com>
2023-03-22 22:46:04 +0530

    update bug fix syntax

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { verifyToken } from "../midelware/jwt.js";
-import { getOrder, createOrder } from "../controllers/order.controller.js";
+import { getOrders, createOrder } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 router.post("/:gigId", verifyToken, createOrder);
-router.get("/", verifyToken, getOrder);
+router.get("/", verifyToken, getOrders);
 
 export default router;
