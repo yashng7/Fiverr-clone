c5b2d83176ac1e066fe190a2aa71374ab00f3a5b
Yashwant Gawande <gawandeyashwant7@gmail.com>
2023-03-23 23:18:28 +0530

    Update conversation controller to sort messages by their arrival

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -48,7 +48,7 @@ export const getConversations = async (req, res, next) => {
   try {
     const conversations = await Conversation.find(
       req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }
-    );
+    ).sort({ updatedAt: -1 });
     res.status(200).send(conversations);
   } catch (err) {
     next(err);
