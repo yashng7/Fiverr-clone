2d4326bdb3d836315dd37c97cce8138a1f374464
Your Name <your_email@example.com>
2023-03-24 23:57:00 +0530

    currency change

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -10,11 +10,12 @@ export const intent = async (req, res, next) => {
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: gig.price * 100,
-    currency: "usd",
+    currency: "inr",
     automatic_payment_methods: {
       enabled: true,
     },
   });
+
   const newOrder = new Order({
     gigId: gig._id,
     img: gig.cover,
@@ -39,5 +40,19 @@ export const getOrders = async (req, res, next) => {
     res.status(200).send(orders);
   } catch (err) {
     next(err);
+  }
+};
+
+export const confirm = async (req, res, next) => {
+  try {
+    const orders = await Order.findOneAndUpdate(
+      {
+        payment_intent: req.body.payment_intent,
+      },
+      { $set: { isCompleted: true } }
+    );
+    res.status(200).send("order has been confirmed");
+  } catch (err) {
+    console.log(err);
   }
 };
