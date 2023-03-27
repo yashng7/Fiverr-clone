8536dec2bae35e84dd0e4e8d05377b3f730eb9e4
Your Name <your_email@example.com>
2023-03-27 17:10:26 +0530

    update

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -1,7 +1,7 @@
 import React, { useReducer, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { gigReducer, INITIAL_STATE } from "../../reducers/gigReducer";
-import { useMutation } from "@tanstack/react-query";
+import { gigReducer, INITIAL_STATE } from "../../reducers/gigReducer.js";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import newRequest from "../../../utils/newRequest.js";
 import "./Add.scss";
 import upload from "../../../utils/upload.js";
@@ -34,7 +34,25 @@ const Add = () => {
     e.target[0].value = "";
   };
 
+  const handleUpload = async () => {
+    setUploading(true);
+    try {
+      const cover = await upload(singleFile);
+      const images = await Promise.all(
+        [...files].map(async (file) => {
+          const url = await upload(file);
+          return url;
+        })
+      );
+      setUploading(false);
+      dispatch({ type: "ADD_IMAGES", payload: { cover, images } });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: (gig) => {
@@ -52,23 +70,6 @@ const Add = () => {
   const handleGigs = (e) => {
     e.preventDefault();
     navigate("/mygigs");
-  };
-
-  const handleUpload = async () => {
-    setUploading(true);
-    try {
-      const cover = await upload(singleFile);
-      const images = await Promise.all(
-        [...files].map(async (file) => {
-          const url = await upload(file);
-          return url;
-        })
-      );
-      setUploading(false);
-      dispatch({ type: "ADD_IMAGES", payload: { cover, images } });
-    } catch (err) {
-      console.log(err);
-    }
   };
 
   return (
@@ -119,7 +120,7 @@ const Add = () => {
               onChange={handleChange}
             ></textarea>
             <button onClick={handleSubmit}>Create</button>
-            <button onClick={handleGigs}>Show MyGigs</button>
+            {/* <button onClick={handleGigs}>Show MyGigs</button> */}
           </div>
           <div className="details">
             <label htmlFor="">Service Title</label>
